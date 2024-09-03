d4c22896bb437cce592c98371c559815cda098ae
Jack Boyd <j.william.boyd@gmail.com>
2024-09-04 00:15:15 +1200

    Connect users to image uploads

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -19,6 +19,7 @@ export const ourFileRouter = {
       await db.insert(images).values({
         name: file.name,
         url: file.url,
+        userId: metadata.userId,
       })
       return { uploadedBy: metadata.userId };
     }),
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,9 @@ import { ClerkProvider } from '@clerk/nextjs'
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 import { TopNav } from "./_components/topnav";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
+import { ourFileRouter } from "~/app/api/uploadthing/core";
 
 export const metadata: Metadata = {
   title: "Boyd Gallery",
@@ -18,6 +21,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" className={`${GeistSans.variable}`}>
+        <NextSSRPlugin
+          routerConfig={extractRouterConfig(ourFileRouter)}
+        />
         <body className="flex flex-col gap-4">
           <TopNav />
           {children}
