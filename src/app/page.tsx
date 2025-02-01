58b6c45e55d25898ff3fa7df7ce9001739c3a32d
Jack Boyd <j.william.boyd@gmail.com>
2025-02-01 15:16:15 +0000

    add auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({ 
     orderBy: (model, { desc }) => desc(model.createdAt),
   });
@@ -17,4 +18,16 @@ export default async function HomePage() {
       </div>
     </main>
   );
+}
+export default async function HomePage() {
+  return (
+    <main className="">
+      <SignedOut>
+        <div className="h-full w-full text-2xl text-center">Please sign in to view the gallery.</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
+    </main>
+  )
 }
