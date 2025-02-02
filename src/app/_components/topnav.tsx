fead566b172919b85f06604a8c0708a23b2ad767
Jack Boyd <j.william.boyd@gmail.com>
2025-02-02 00:03:12 +0000

    Add upload

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,13 +1,29 @@
-import { SignInButton, SignedOut, SignedIn, UserButton } from "@clerk/nextjs";
+"use client";
+
+import { SignInButton, SignedOut, SignedIn, UserButton, useUser } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { UploadButton } from "~/utils/uploadthing";
+
 export function TopNav() {
+  const { user } = useUser();
+  const router = useRouter();
+  console.log("user", user);
+
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <div>Gallery</div>
-      <div>
+      <div className="flex flex-row ">
         <SignedOut>
           <SignInButton />
         </SignedOut>
         <SignedIn>
+        {user?.primaryEmailAddress?.emailAddress === "j.william.boyd@gmail.com" && "fuck you"}
+          <UploadButton 
+            endpoint="imageUploader" 
+            onClientUploadComplete={() => {
+              router.refresh();
+            }}
+          />
           <UserButton />
         </SignedIn>
       </div>
