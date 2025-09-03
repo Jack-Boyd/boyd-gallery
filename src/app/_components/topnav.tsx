f94e6f56f8f069e5acff1f53d3cdc4ade7cd388a
Jack Boyd <j.william.boyd@gmail.com>
2025-09-03 17:40:53 +0100

    commit top nav

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -4,7 +4,6 @@ import { SignInButton, SignedOut, SignedIn, UserButton } from "@clerk/nextjs";
 import { SimpleUploadButton } from "./upload-button";
 
 export function TopNav() {
-
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <div>Hannah & Jack Gallery</div>
